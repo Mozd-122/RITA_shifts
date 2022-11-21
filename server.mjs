26c60cea1cd6d84be674595a63075209afd1885b
Mozzdy <74615838+Mozd-122@users.noreply.github.com>
2022-11-21 17:03:52 -0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,25 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
+  if(req.method == "GET")
+    {
+      res.write('received GET request.');
+        res.end("received GET request.")
+    }
+else if(req.method == "POST")
+    {
+      res.write('received POST request.');
+        res.end("received POST request.");
+    }
+else
+    {
+      res.write('Undefined request .');
+        res.end("Undefined request .");
+    }
+});
+  
+  
+  
   res.write('Hello World!');
   res.end();
 }).listen(process.env.PORT);
